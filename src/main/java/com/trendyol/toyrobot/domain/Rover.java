be3de4db9fe4e4f035115968e14e905dbbcb02fd
Fırat Eşki <frat888@gmail.com>
2020-10-01 04:31:47 +0300

    Update Rover based on refactoring

--- src/main/java/com/trendyol/toyrobot/domain/Rover.java
+++ src/main/java/com/trendyol/toyrobot/domain/Rover.java
@@ -1,68 +1,41 @@
 package com.trendyol.toyrobot.domain;
 
+import com.trendyol.toyrobot.domain.compass.Compass;
+import com.trendyol.toyrobot.domain.compass.Location;
+import com.trendyol.toyrobot.domain.compass.North;
+
 public class Rover {
 
-    private int x;
-    private int y;
+    private Location location;
     private Compass compass;
 
     public Rover() {
-        this.x = 0;
-        this.y = 0;
-        this.compass = Compass.NORTH;
+        this.location = new Location(0, 0);
+        this.compass = new North();
     }
 
     public Rover(int x, int y, Compass compass) {
-        this.x = x;
-        this.y = y;
+        location = new Location(x, y);
         this.compass = compass;
     }
 
     public void move() {
-        if (Compass.NORTH.equals(this.compass)) {
-            this.y = this.y + 1;
-        } else if (Compass.EAST.equals(this.compass)) {
-            this.x = this.x + 1;
-        } else if (Compass.SOUTH.equals(this.compass)) {
-            this.y = this.y - 1;
-        } else if (Compass.WEST.equals(this.compass)) {
-            this.x = this.x - 1;
-        }
+        location = compass.move(location);
     }
 
     public void turnLeft() {
-        if (Compass.NORTH.equals(this.compass)) {
-            this.compass = Compass.WEST;
-        } else if (Compass.WEST.equals(this.compass)) {
-            this.compass = Compass.SOUTH;
-        } else if (Compass.SOUTH.equals(this.compass)) {
-            this.compass = Compass.EAST;
-        } else if (Compass.EAST.equals(this.compass)) {
-            this.compass = Compass.NORTH;
-        }
+        this.compass = compass.turnLeft();
     }
 
     public void turnRight() {
-        if (Compass.NORTH.equals(this.compass)) {
-            this.compass = Compass.EAST;
-        } else if (Compass.EAST.equals(this.compass)) {
-            this.compass = Compass.SOUTH;
-        } else if (Compass.SOUTH.equals(this.compass)) {
-            this.compass = Compass.WEST;
-        } else if (Compass.WEST.equals(this.compass)) {
-            this.compass = Compass.NORTH;
-        }
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY() {
-        return this.y;
+        this.compass = compass.turnRight();
     }
 
     public Compass getCompass() {
         return this.compass;
+    }
+
+    public Location getLocation(){
+        return location;
     }
 }
